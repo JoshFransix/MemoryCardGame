eb2d8e84fc9f9ee37a50842912bfe37f525e1a4e
joshfransix <joshiandersonk69@gmail.com>
2022-03-26 08:47:11 +0100

    addedClickEvent

---
+++ main.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll(".memory-card");
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // first Click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second Click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    console.log({ firstCard, secondCard });
+  }
+}
