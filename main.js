bec7a10eb80cbe17dae384e3b9dd2bf054ac6ede
joshfransix <joshiandersonk69@gmail.com>
2022-03-26 10:36:43 +0100

    finalCommit

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const cards = document.querySelectorAll(".memory-card");
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.add("flip");
 
   if (!hasFlippedCard) {
@@ -16,7 +20,7 @@ function flipCard() {
     return;
   }
   // second Click
-  hasFlippedCard = false;
+
   secondCard = this;
 
   // do cards match
@@ -32,13 +36,30 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
   // not a match
+  lockBoard = true;
 
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+
+    resetBoard();
   }, 1500);
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffleBoard() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * cards.length);
+    card.style.order = randomPos;
+  });
+})();
